b1c24fcdd8318d51cc7b201b646d0f061c966430
Andrés Cabeza <andres.cabeza@idrd.gov.co>
2025-03-24 18:22:51 -0500

    feat: class 38 changes on input

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChange } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { ProductComponent } from "../../../products/components/product/product.component";
 
 @Component({
@@ -20,11 +20,15 @@ export class CounterComponent {
     console.log('-'.repeat(10));
   }
 
-  ngOnChanges(changes: SimpleChange){
+  ngOnChanges(changes: SimpleChanges){
     // before and during render
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
     console.log(changes);
+    const duration = changes['duration'];
+    if (duration && duration.currentValue !== duration.previousValue) {
+      this.doSomething();
+    }
   }
 
   ngOnInit() {
@@ -48,5 +52,9 @@ export class CounterComponent {
     // con un ngIf
     console.log('ngOnDestroy');
     console.log('-'.repeat(10));
+  }
+
+  doSomething() {
+    console.log('change duration');
   }
 }
