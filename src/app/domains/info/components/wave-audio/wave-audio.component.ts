01d8329dfeb4ddcab5634fc88056946fa87aab08
Andrés Cabeza <andres.cabeza@idrd.gov.co>
2025-03-24 20:37:45 -0500

    feat: class-41 about-us

--- src/app/domains/info/components/wave-audio/wave-audio.component.ts
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -1,10 +1,11 @@
-import { Component, ElementRef, Input, SimpleChanges, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Input, signal, SimpleChanges, ViewChild } from '@angular/core';
 import WaveSurfer from 'wavesurfer.js';
 
 @Component({
   selector: 'app-wave-audio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './wave-audio.component.html',
   styleUrl: './wave-audio.component.css'
 })
@@ -12,11 +13,23 @@ export class WaveAudioComponent {
 
   @Input({required:true}) audioUrl!:string;
   @ViewChild('wave') container!: ElementRef;
+  private ws!: WaveSurfer;
+  isPlaying = signal(false);
 
   ngAfterViewInit(changes: SimpleChanges){
-    WaveSurfer.create({
+    this.ws = WaveSurfer.create({
       url: this.audioUrl,
       container: this.container.nativeElement
     });
+    this.ws.on('play', () => {this.isPlaying.set(true)});
+
+    this.ws.on('pause', () => {this.isPlaying.set(false)});
+
+  }
+  playPause(){
+    this.ws.playPause();
+  }
+  ngOnDestroy(){
+    this.ws.destroy();
   }
 }
