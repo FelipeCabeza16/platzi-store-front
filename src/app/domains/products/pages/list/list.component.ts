c360678285e73728825386ca366dd57f6f806c1c
Andrés Cabeza <andres.cabeza@idrd.gov.co>
2025-03-29 13:29:51 -0500

    feat: class 50 connect front + API REST

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -4,6 +4,7 @@ import { Product } from '../../../shared/models/product.model';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "../../../shared/components/header/header.component";
 import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -16,97 +17,20 @@ export class ListComponent {
 
   products = signal<Product[]>([]);
   private cartService = inject(CartService);
+  private productService = inject(ProductService);
 
-  constructor() {
-    const initProducts:Product[] = [
-      {
-        id: Date.now(),
-        title: 'product 1',
-        price: 5000,
-        image: 'https://picsum.photos/640/640?r=23',
-        creationAt: new Date().toISOString()
+  ngOnInit(){
+    this.productService.getProducts()
+    .subscribe({
+      next: (products) => {
+        this.products.set(products);
       },
-      {
-        id: Date.now() + 1,
-        title: 'product 2',
-        price: 3000,
-        image: 'https://picsum.photos/640/640?r=24',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now() + 2,
-        title: 'product 3',
-        price: 4500,
-        image: 'https://picsum.photos/640/640?r=25',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now() + 3,
-        title: 'product 4',
-        price: 7000,
-        image: 'https://picsum.photos/640/640?r=26',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now() + 4,
-        title: 'product 5',
-        price: 2500,
-        image: 'https://picsum.photos/640/640?r=27',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now() + 5,
-        title: 'product 6',
-        price: 6000,
-        image: 'https://picsum.photos/640/640?r=28',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'product 1',
-        price: 5000,
-        image: 'https://picsum.photos/640/640?r=23',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now() + 1,
-        title: 'product 2',
-        price: 3000,
-        image: 'https://picsum.photos/640/640?r=24',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now() + 2,
-        title: 'product 3',
-        price: 4500,
-        image: 'https://picsum.photos/640/640?r=25',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now() + 3,
-        title: 'product 4',
-        price: 7000,
-        image: 'https://picsum.photos/640/640?r=26',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now() + 4,
-        title: 'product 5',
-        price: 2500,
-        image: 'https://picsum.photos/640/640?r=27',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now() + 5,
-        title: 'product 6',
-        price: 6000,
-        image: 'https://picsum.photos/640/640?r=28',
-        creationAt: new Date().toISOString()
-      },
-    ];
-    this.products.set(initProducts);
+      error: () => {
+
+      }
+    },
+  );
   }
-
 
   addToCart(product: Product){
     this.cartService.addToCart(product);
