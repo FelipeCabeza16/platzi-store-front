ac56f0c3f475ff190d44764d64d9dbc8fae5fa09
Andrés Cabeza <andres.cabeza@idrd.gov.co>
2025-03-29 20:42:49 -0500

    feat: class-62 fetch product

--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -10,8 +10,10 @@ export class ProductService {
   private http = inject(HttpClient)
   constructor() { }
 
-
   getProducts() {
     return this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products');
+  }
+  getOne(id: string) {
+    return this.http.get<Product>('https://api.escuelajs.co/api/v1/products/' + id );
   }
 }
