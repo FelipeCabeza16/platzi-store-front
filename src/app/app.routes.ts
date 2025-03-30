f3f20ab0a41fa7d6664d3f6633183a5f7f497544
Andrés Cabeza <andres.cabeza@idrd.gov.co>
2025-03-29 22:14:38 -0500

    feat: class-69 prefetching

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
-import { ListComponent } from './domains/products/pages/list/list.component';
-import { AboutComponent } from './domains/info/pages/about/about.component';
 import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
 import { LayoutComponent } from '@shared/components/layout/layout.component';
-import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
 
 export const routes: Routes = [
   {
